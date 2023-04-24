7a34836bf3d053c61685a2384d456d8fe9009065
dirtydenny <dallen7@socal.rr.com>
2023-04-24 13:56:06 -0700

    first index skeleton

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -15,3 +15,5 @@ export default function About() {
     </div>
   );
 }
+
+export default About;
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Home() {
+export default function Nav() {
   return (
     <div>
       <h1>Home Page</h1>
